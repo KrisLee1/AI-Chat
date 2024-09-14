f338c78ea3f139639158aa08f041622ed127ad7d
KrisLee1 <74817528+KrisLee1@users.noreply.github.com>
2024-09-15 01:18:12 +0800

    Initial commit

---
+++ app/components/RangeInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <UFormGroup :label="label" :ui="{ container: 'mt-2' }">
+    <template #hint>
+      <UInput
+        v-model="model"
+        class="w-[72px]"
+        type="number"
+        :min="min"
+        :max="max"
+        :step="step"
+      />
+    </template>
+    <URange :key="`${label}-${rangeKey}`" v-model="model" :min="min" :max="max" :step="step" size="sm" />
+  </UFormGroup>
+</template>
+
+<script setup lang="ts">
+const model = defineModel({ type: Number, default: undefined });
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  min: {
+    type: Number,
+    default: undefined,
+  },
+  max: {
+    type: Number,
+    default: undefined,
+  },
+  step: {
+    type: Number,
+    default: undefined,
+  },
+});
+
+const rangeKey = ref(0);
+onMounted(() => {
+  rangeKey.value++;
+});
+</script>
